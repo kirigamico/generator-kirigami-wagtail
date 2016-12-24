e4abb810c8950d244540d2df68632eb9e083c14b
Mitchel Cabuloy <mixxorz@gmail.com>
2016-12-24 20:01:32 +0800

    Add pip install step

--- generators/app/index.js
+++ generators/app/index.js
@@ -33,9 +33,27 @@ module.exports = Generator.extend({
     );
 
     this.fs.copyTpl(
+      this.templatePath('manage.py'),
+      this.destinationPath('manage.py'),
+      {projectName: this.props.pyname}
+    );
+
+    this.fs.copyTpl(
       this.templatePath('project_name/**/*'),
       this.destinationPath(this.props.pyname),
       {projectName: this.props.pyname}
     );
+  },
+
+  install: function () {
+    // this.installDependencies();
+
+    if (process.env.VIRTUAL_ENV) {
+      this.log(chalk.yellow('Also installing Python dependencies with pip.'));
+      this.spawnCommandSync('pip', ['install', 'pip-tools']);
+      this.spawnCommandSync('pip-sync', ['requirements.txt', 'dev-requirements.txt']);
+    } else {
+      this.log(chalk.yellow('Not in a virtualenv, skipping pip install.'));
+    }
   }
 });
