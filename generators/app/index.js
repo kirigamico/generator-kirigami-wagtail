ff69097e586234fb42f79b3ec1ccf83df903061c
Mitchel Cabuloy <mixxorz@gmail.com>
2016-12-24 18:51:46 +0800

    Add wagtail starter template

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,41 @@
 'use strict';
-var Generator = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const path = require('path');
+const yosay = require('yosay');
 
 module.exports = Generator.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the divine ' + chalk.red('generator-kirigami-wagtail') + ' generator!'
+      'Welcome to the ' + chalk.red('kirigami-wagtail') + ' generator!'
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: path.basename(process.cwd())
     }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
+      this.props.pyname = this.props.name.replace(/-/g, '_');
     }.bind(this));
   },
 
-  writing: function () {
+  python: function () {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('**/*'),
+      this.destinationPath(),
+      {globOptions: {ignore: '**/*/project_name/*'}}
     );
-  },
 
-  install: function () {
-    this.installDependencies();
+    this.fs.copyTpl(
+      this.templatePath('project_name/**/*'),
+      this.destinationPath(this.props.pyname),
+      {projectName: this.props.pyname}
+    );
   }
 });
