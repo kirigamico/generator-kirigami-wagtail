a3f2a544407582c656be16390dc377e3aa4170ca
Mitchel Cabuloy <mixxorz@gmail.com>
2018-07-04 14:51:56 +0800

    Upgrade to Wagtail 2.0

--- generators/app/templates/project_name/urls.py
+++ generators/app/templates/project_name/urls.py
@@ -1,23 +1,17 @@
-from __future__ import absolute_import, unicode_literals
-
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
 from apps.search import views as search_views
-from wagtail.wagtailadmin import urls as wagtailadmin_urls
-from wagtail.wagtailcore import urls as wagtail_urls
-from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 urlpatterns = [
-    url(r'^django-admin/', include(admin.site.urls)),
-
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^django-admin/', admin.site.urls),
+    url(r'^admin/', include('wagtail.admin.urls')),
 
     url(r'^search/$', search_views.search, name='search'),
 
-    url(r'', include(wagtail_urls)),
+    url(r'^documents/', include('wagtail.documents.urls')),
+    url(r'', include('wagtail.core.urls')),
 ]
 
 
